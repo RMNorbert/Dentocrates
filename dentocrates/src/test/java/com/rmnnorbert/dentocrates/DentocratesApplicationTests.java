7d4cd0b8f401f3174ad948e889e65e3a54d046a1
Norbert Remenyi <remenyinorbert@gmail.com>
2023-07-30 19:57:27 +0200

    chore : remove @SpringBootTest annotation from test class

--- dentocrates/src/test/java/com/rmnnorbert/dentocrates/DentocratesApplicationTests.java
+++ dentocrates/src/test/java/com/rmnnorbert/dentocrates/DentocratesApplicationTests.java
@@ -2,11 +2,9 @@ package com.rmnnorbert.dentocrates;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
 class DentocratesApplicationTests {
 
 	@Test
