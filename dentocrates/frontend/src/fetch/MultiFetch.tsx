8bc3931f2e3b7a1d439426ff7eaebcca1a7b5964
Norbert Remenyi <remenyinorbert@gmail.com>
2023-09-10 18:56:56 +0200

    feat: change MultiFetch and NavigationBar from JavaScript to Typescript

--- dentocrates/frontend/src/fetch/MultiFetch.tsx
+++ dentocrates/frontend/src/fetch/MultiFetch.tsx
@@ -1,20 +1,35 @@
 import { token } from "../components/token/TokenDecoder";
 import { useState } from "react";
 
-export const MultiFetch = () => {
-    const [isLoading, setLoading] = useState(true);
-    const data = async (url, method, answerObject) => {
+export interface ApiResponse {
+    data: any;
+    status: number;
+    message: string;
+}
 
+interface FetchResult {
+    data: (url: string, method?: string, answerObject?: any) => Promise<any>;
+    isLoading: boolean;
+}
+
+export const MultiFetch = (): FetchResult => {
+    const [isLoading, setLoading] = useState<boolean>(true);
+
+    const data = async (
+        url: string,
+        method?: string,
+        answerObject?: any
+    ): Promise<ApiResponse> => {
         try {
             setLoading(true);
-            const headers = token() ?
-                {
+            const headers: Record<string, string> = token()
+                ? {
                     "Content-Type": "application/json",
                     "Authorization": `Bearer ${token()}`,
-                } :
-                {
-                    "Content-Type": "application/json",
                 }
+                : {
+                    "Content-Type": "application/json",
+                };
             const response = await fetch((url[0] === "/" ? "" : "/") + url, {
                 method: method ?? "GET",
                 body: answerObject ? JSON.stringify(answerObject) : undefined,
@@ -28,13 +43,7 @@ export const MultiFetch = () => {
                 console.error(errorMessage);
                 throw new Error(errorMessage);
             }
-
-            const contentType = response.headers.get("content-type");
-            if (contentType && contentType.includes("application/json")) {
-                return await response.json();
-            } else {
-                return await response.text();
-            }
+            return await response.json();
         } catch (error) {
             console.error(error);
             throw error;
@@ -43,4 +52,3 @@ export const MultiFetch = () => {
 
     return { data, isLoading };
 };
-
