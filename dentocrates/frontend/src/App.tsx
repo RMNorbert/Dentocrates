24a4d07d0383a054026d0eb24e058510cf5ec53f
Norbert Remenyi <remenyinorbert@gmail.com>
2023-09-10 18:46:55 +0200

    feat: change App.js to Typescript

--- dentocrates/frontend/src/App.tsx
+++ dentocrates/frontend/src/App.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import LoginPage from "./components/client/LoginPage";
 import RegisterPage from "./components/client/RegisterPage";
 import Home from "./components/Home";
 import NavigationBar from "./components/NavigationBar";
 import {ClientPage} from "./components/client/ClientPage";
 import {DentistPage} from "./components/client/DentistPage";
-import {ClinicSelectorPage} from "./components/clinic/ClinicSelectorPage";
+import { ClinicSelectorPage } from './components/clinic/ClinicSelectorPage';
 import { ClinicPage } from "./components/clinic/ClinicPage";
 import LocationRegisterPage from "./components/clinic/location/LocationRegisterPage";
-import ClinicRegisterPage from "./components/clinic/ClinicRegisterPage.jsx";
+import ClinicRegisterPage from "./components/clinic/ClinicRegisterPage";
 import Calendar from "./components/clinic/calendar/Calendar";
 import {Appointment} from "./components/client/Appointment";
 import VerifyPage from "./components/client/Verification";
@@ -30,7 +31,7 @@ function App() {
           <Route path="/calendar/:id" element={<Calendar />} />
           <Route path="/location" element={<LocationRegisterPage />} />
           <Route path="/clinic/register" element={<ClinicRegisterPage />} />
-          <Route path="/verify/:verificationCode" element={<VerifyPage reset={false} />} />
+          <Route path="/verify/:verificationCode" element={<VerifyPage />} />
           <Route path="/verify/reset/:verificationCode" element={<VerifyPage reset={true} />} />
           <Route path="/login/oauth2/code" element={<OauthLoginPage />} />
         </Routes>
