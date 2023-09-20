44b6f07d4d1fb8c11f4ee8da9f5f9b262c4f0ce6
Norbert Remenyi <remenyinorbert@gmail.com>
2023-09-20 13:36:30 +0200

    chore: relocate client related update methods to ClientUpdaterService

---
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/service/client/ClientUpdaterService.java
@@ -0,0 +1,61 @@
+package com.rmnnorbert.dentocrates.service.client;
+
+import com.rmnnorbert.dentocrates.controller.dto.client.update.ResetDto;
+import com.rmnnorbert.dentocrates.controller.dto.client.update.VerifyDto;
+import com.rmnnorbert.dentocrates.custom.exceptions.InvalidCredentialException;
+import com.rmnnorbert.dentocrates.dao.verification.Verification;
+import com.rmnnorbert.dentocrates.data.Role;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientUpdaterService {
+    private final VerificationService verificationService;
+    private final CustomerService customerService;
+    private final DentistService dentistService;
+    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    public ClientUpdaterService(VerificationService verificationService, CustomerService customerService, DentistService dentistService, PasswordEncoder passwordEncoder) {
+        this.verificationService = verificationService;
+        this.customerService = customerService;
+        this.dentistService = dentistService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public ResponseEntity<String> resetPassword(ResetDto dto) {
+        Verification verification = verificationService.getVerification(dto.verificationCode());
+
+        if(verification.getEmail().equals(dto.email())) {
+            updateClientPassword(verification, dto.password());
+            return ResponseEntity.ok().body("Password changed successfully.");
+        } else {
+            throw new InvalidCredentialException();
+        }
+    }
+
+    public ResponseEntity<String> verifyClient(VerifyDto dto) {
+        Verification verification = verificationService.getVerification(dto.verificationCode());
+
+        if(verification.getRole().equals(Role.CUSTOMER)) {
+            customerService.verifyCustomer(verification.getEmail());
+        }
+        else if (verification.getRole().equals(Role.DENTIST)) {
+            dentistService.verifyDentist(verification.getEmail());
+        }
+        return ResponseEntity.ok().body("Verification successful.");
+    }
+    private void updateClientPassword(Verification verification, String password ) {
+        String newPassword = passwordEncoder.encode(password);
+
+        if (verification.getRole().equals(Role.CUSTOMER)) {
+            customerService.updateCustomerPassword(verification.getEmail(), newPassword);
+        }
+
+        else if (verification.getRole().equals(Role.DENTIST)) {
+            dentistService.updateDentistPassword(verification.getEmail(), newPassword);
+        }
+    }
+
+}
