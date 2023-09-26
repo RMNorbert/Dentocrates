1ed31d85e4c89f0f8297c69581378f3bd8d980e2
Norbert Remenyi <remenyinorbert@gmail.com>
2023-09-26 15:59:03 +0200

    chore: change MultiFetch.tsx parameters and remove redundant interface

--- dentocrates/frontend/src/fetch/MultiFetch.tsx
+++ dentocrates/frontend/src/fetch/MultiFetch.tsx
@@ -1,54 +1,30 @@
 import { token } from "../components/token/TokenDecoder";
-import { useState } from "react";
 
-export interface ApiResponse {
-    data: any;
-    status: number;
-    message: string;
-}
 
-interface FetchResult {
-    data: (url: string, method?: string, answerObject?: any) => Promise<any>;
-    isLoading: boolean;
-}
-
-export const MultiFetch = (): FetchResult => {
-    const [isLoading, setLoading] = useState<boolean>(true);
-
-    const data = async (
-        url: string,
-        method?: string,
-        answerObject?: any
-    ): Promise<ApiResponse> => {
-        try {
-            setLoading(true);
-            const headers: Record<string, string> = token()
-                ? {
-                    "Content-Type": "application/json",
-                    "Authorization": `Bearer ${token()}`,
-                }
-                : {
-                    "Content-Type": "application/json",
-                };
-            const response = await fetch((url[0] === "/" ? "" : "/") + url, {
-                method: method ?? "GET",
-                body: answerObject ? JSON.stringify(answerObject) : undefined,
-                headers: headers,
-            });
-
-            if (response.ok) {
-                setLoading(false);
-            } else {
-                const errorMessage = `Failed to ${method ?? "GET"} to table: ${url}`;
-                console.error(errorMessage);
-                throw new Error(errorMessage);
+export async function MultiFetch<T>(url: string, method?: string, answerObject?: any): Promise<T> {
+    try {
+        const headers: Record<string, string> = token()
+            ? {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${token()}`,
             }
-            return await response.json();
-        } catch (error) {
-            console.error(error);
-            throw error;
-        }
-    };
+            : {
+                "Content-Type": "application/json",
+            };
+        const response = await fetch((url[0] === "/" ? "" : "/") + url, {
+            method: method ?? "GET",
+            body: answerObject ? JSON.stringify(answerObject) : undefined,
+            headers: headers,
+        });
 
-    return { data, isLoading };
-};
+        if (!response.ok) {
+            const errorMessage = `Failed to ${method ?? "GET"} to table: ${url}`;
+            console.error(errorMessage);
+            throw new Error(errorMessage);
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
