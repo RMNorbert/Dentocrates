39d1b89f8d4d4e08777d4e9496fe85374b9f7331
Norbert Remenyi <remenyinorbert@gmail.com>
2023-10-15 17:00:39 +0200

    feat: Add Review controller for handling incoming requests for review.

---
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/ReviewController.java
@@ -0,0 +1,40 @@
+package com.rmnnorbert.dentocrates.controller;
+
+import com.rmnnorbert.dentocrates.dto.DeleteDTO;
+import com.rmnnorbert.dentocrates.dto.review.ReviewDTO;
+import com.rmnnorbert.dentocrates.dto.review.ReviewRegisterDTO;
+import com.rmnnorbert.dentocrates.service.review.ReviewService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/review")
+public class ReviewController {
+    private final ReviewService reviewService;
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @GetMapping("/customer/{id}")
+    public List<ReviewDTO> getCustomerReviews(@PathVariable Long id ){
+        return reviewService.getAllReviewById(id);
+    }
+    @GetMapping("/clinic/{id}")
+    public List<ReviewDTO> getReviewsByClinic(@PathVariable Long id ){
+        return reviewService.getAllReviewByClinic(id);
+    }
+    @PostMapping("/register")
+    public ResponseEntity<String> addReview(@Valid @RequestBody ReviewRegisterDTO dto){
+        return reviewService.registerReview(dto);
+    }
+    @DeleteMapping("/")
+    public ResponseEntity<String> removeReview(@Valid @RequestBody DeleteDTO dto){
+        return reviewService.deleteReviewById(dto);
+    }
+
+}
