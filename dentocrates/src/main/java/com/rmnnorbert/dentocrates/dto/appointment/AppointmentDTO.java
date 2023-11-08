048b290e48ebac5f13864bd002277e222e9e943a
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-08 17:13:51 +0100

    chore: add reviewed field to AppointmentDTO and update import path according to project structure changes

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/dto/appointment/AppointmentDTO.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/dto/appointment/AppointmentDTO.java
@@ -1,6 +1,6 @@
 package com.rmnnorbert.dentocrates.dto.appointment;
 
-import com.rmnnorbert.dentocrates.dao.clinic.AppointmentCalendar;
+import com.rmnnorbert.dentocrates.dao.appointmentCalendar.AppointmentCalendar;
 import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
@@ -11,7 +11,8 @@ public record AppointmentDTO(@Min(1) long id,
                              @Min(1) long clinicId,
                              @Min(1) long customerId,
                              LocalDateTime reservation,
-                             boolean appeared
+                             boolean appeared,
+                             boolean reviewed
                             ) {
     public static AppointmentDTO of(AppointmentCalendar calendar) {
         return AppointmentDTO.builder()
@@ -20,6 +21,7 @@ public record AppointmentDTO(@Min(1) long id,
                 .clinicId(calendar.getClinic().getId())
                 .reservation(calendar.getReservation())
                 .appeared(calendar.isAppeared())
+                .reviewed(calendar.isReviewed())
                 .build();
     }
 }
