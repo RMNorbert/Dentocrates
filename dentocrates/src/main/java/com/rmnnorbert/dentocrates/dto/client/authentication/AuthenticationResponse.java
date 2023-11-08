17f050d24635e0b77e50ca734018b98079183140
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-08 17:14:25 +0100

    chore: add id validation to AuthenticationResponse

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/dto/client/authentication/AuthenticationResponse.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/dto/client/authentication/AuthenticationResponse.java
@@ -1,8 +1,9 @@
 package com.rmnnorbert.dentocrates.dto.client.authentication;
 
+import jakarta.validation.constraints.Min;
 import lombok.Builder;
 
 
 @Builder
-public record AuthenticationResponse(String token, long id) {
+public record AuthenticationResponse(String token,@Min(1) long id) {
 }
