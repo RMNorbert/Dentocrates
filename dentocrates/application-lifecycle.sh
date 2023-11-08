13c08fbfc2b8b828fa43fffd3032d3b1d1da9593
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-08 19:29:25 +0100

    chore: extend application-lifecycle.sh file with environment variables

--- dentocrates/application-lifecycle.sh
+++ dentocrates/application-lifecycle.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-# Log file path
-LOG_FILE="your_absolute_path_to/Dentocrates/logfile.log"
+
+PROJECT_DIR="$(dirname "$(readlink -f "$0")")"
+LOG_DIR="$PROJECT_DIR/.."
+LOG_FILE="$LOG_DIR/logfile.log"
+NOT_FOUND="Process not found on port"
+DIRECTORY_CHANGE="Changing directory to "
+DIRECTORY_CHANGE_ERROR="Error: Unable to change directory to "
+STARTING="Starting "
+SHUTTING_DOWN="Shutting down "
 
 function log_message() {
     local timestamp
@@ -10,7 +17,7 @@ function log_message() {
 
 # Function to handle Ctrl+C (SIGINT)
 function shutdown_backend() {
-    log_message "Shutting down Backend..."
+    log_message "$SHUTTING_DOWN Backend"
     # Find the PID of the process listening on port 8080
     pid=$(lsof -t -i :8080)
 
@@ -24,43 +31,27 @@ function shutdown_backend() {
         log_message "Script completed successfully."
         exit 0
     else
-        log_message "Process not found on port 8080."
+        log_message "$NOT_FOUND 8080."
         exit 1
     fi
 }
 
-function shutdown_proxy_server() {
-    log_message "Shutting down proxy server..."
-      proxy_pid=$(lsof -t -i :3001)
-
-      if [ -n "$proxy_pid" ]; then
-          log_message "Found PID: $proxy_pid"
-
-          kill -15 $proxy_pid
-          # Shutdown proxy server
-          shutdown_backend
-      else
-          log_message "Process not found on port 3001."
-          shutdown_backend
-      fi
-}
-
 function shutdown_frontend() {
-    log_message "Shutting down Frontend..."
+    log_message "$SHUTTING_DOWN Frontend"
     frontend_pid=$(lsof -t -i :3000)
 
     if [ -n "$frontend_pid" ]; then
         log_message "Found PID: $frontend_pid"
         kill -15 $frontend_pid
-        shutdown_proxy_server
+        shutdown_backend
     else
-        log_message "Process not found on port 3000."
-        shutdown_proxy_server
+        log_message "$NOT_FOUND 3000."
+        shutdown_backend
     fi
 }
 
 function shutdown_chat_bot() {
-  log_message "Shutting down ChatBot..."
+  log_message "$SHUTTING_DOWN ChatBot"
   chat_pid=$(lsof -t -i :5000)
 
   if [ -n "$chat_pid" ]; then
@@ -68,7 +59,7 @@ function shutdown_chat_bot() {
       kill -15 $chat_pid
       shutdown_frontend
   else
-      log_message "Process not found on port 5000."
+      log_message "$NOT_FOUND 5000."
       shutdown_frontend
   fi
 }
@@ -81,15 +72,16 @@ export JAVA_HOME="/usr/lib/jvm/java-17-openjdk-amd64" # replace with your path t
 export EMAIL="dentocrates@dentocrates.com"
 export oauthId=your_oauth_id
 export oauthSecret=your_oauth_secret
-export SECRET=your_jwt_secret
-export SENDER_USERNAME=your_email_sender_authenticating_username
+export SECRET=048492546432436A442848F3147319D665857603B6B272848F751725773475237A5E567A66564
+export SENDER_USERNAME=your_email_sender_email_address
+export REDIRECT_URI=http://localhost:3000/login/oauth2/code/
 
-BACKEND_DIR="your_absolute_path_to/Dentocrates/dentocrates/"
-FRONTEND_DIR="your_absolute_path_to/Dentocrates/dentocrates/frontend/"
-CHAT_BOT_DIR="your_absolute_path_to/ConvoCat/"
+
+FRONTEND_DIR="$PROJECT_DIR/frontend/"
+CHAT_BOT_DIR="$PROJECT_DIR/../ConvoCat/"
 # Change to the backend directory
-log_message "Changing to the backend directory..."
-cd "$BACKEND_DIR" || { log_message "Error: Unable to change to backend directory."; exit 1; }
+log_message "$DIRECTORY_CHANGE backend..."
+cd "$PROJECT_DIR" || { log_message "$DIRECTORY_CHANGE_ERROR backend."; exit 1; }
 
 # Generate the Maven Wrapper (if not already generated)
 if [ ! -f "mvnw" ]; then
@@ -102,24 +94,22 @@ fi
 log_message "Cleaning and building the backend..."
 ./mvnw clean install -Dmaven.test.skip=true
 
-log_message "Starting Backend..."
+log_message "$STARTING Backend..."
 ./mvnw spring-boot:run &
 
 # Sleep for a few seconds to allow the backend to start
 sleep 10
 
 # Change to the frontend directory
-log_message "Changing to the frontend directory..."
-cd "$FRONTEND_DIR" || { log_message "Error: Unable to change to frontend directory."; shutdown_backend; }
+log_message "$DIRECTORY_CHANGE frontend..."
+cd "$FRONTEND_DIR" || { log_message "$DIRECTORY_CHANGE_ERROR frontend."; shutdown_backend; }
 
 # Start the frontend (React)
-log_message "Starting Frontend..."
+log_message "$STARTING Frontend..."
 npm start &
-log_message "Starting Proxy Server..."
-npm run proxy &
 
-cd "$CHAT_BOT_DIR" || { log_message "Error: Unable to change to chatbot directory."; shutdown_frontend; }
-log_message "Starting ChatBot..."
+cd "$CHAT_BOT_DIR" || { log_message "$DIRECTORY_CHANGE_ERROR chatbot."; shutdown_frontend; }
+log_message "$STARTING ChatBot..."
 python3 app.py &
 wait
 
