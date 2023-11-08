6b63acc7251cf2798f6a8dcd2c82e5dfa4d98180
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-08 17:22:24 +0100

    feat: add ChatService to handle the logic behind requests between frontend and chat service

---
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/service/chat/ChatService.java
@@ -0,0 +1,45 @@
+package com.rmnnorbert.dentocrates.service.chat;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rmnnorbert.dentocrates.dto.chat.ChatDTO;
+import org.springframework.http.*;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class ChatService {
+    private static final String PYTHON_SERVER_URL = System.getenv("CHAT_URL");
+    private static final String ANSWER_RESPONSE_KEY = "answer";
+    public ResponseEntity<String> getChatResponse(ChatDTO input){
+     HttpHeaders headers = new HttpHeaders();
+     headers.setContentType(MediaType.APPLICATION_JSON);
+
+     HttpEntity<ChatDTO> requestEntity = new HttpEntity<>(input, headers);
+
+     RestTemplate restTemplate = new RestTemplate();
+
+        try {
+        ResponseEntity<String> response = restTemplate.exchange(
+                PYTHON_SERVER_URL,
+                HttpMethod.POST,
+                requestEntity,
+                String.class
+        );
+        if (response.getStatusCode() == HttpStatus.OK) {
+            ObjectMapper objectMapper = new ObjectMapper();
+            Map<String, String> responseMap = objectMapper.readValue(response.getBody(), new TypeReference<>() {});
+
+            String answer = responseMap.get(ANSWER_RESPONSE_KEY);
+
+            return new ResponseEntity<>(answer, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Error: " + response.getStatusCodeValue(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    } catch (Exception e) {
+        return new ResponseEntity<>("Error: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+ }
+}
