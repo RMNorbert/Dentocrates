0cbf7c210e558f7012ae632b9dd4b8626f3a7608
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-12 20:42:24 +0100

    feat: display error messages of login page on client side

--- dentocrates/frontend/src/components/client/authentication/login/LoginPage.jsx
+++ dentocrates/frontend/src/components/client/authentication/login/LoginPage.jsx
@@ -5,11 +5,13 @@ import {MultiFetch} from "../../../../utils/fetch/MultiFetch";
 import { handleGoogleLogin } from "../oauth/OauthLogin";
 
 function LoginPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [authenticationCode, setAuthenticationCode] = useState('');
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [authenticationCode, setAuthenticationCode] = useState("");
     const [clientIsValid, setClientIsValid] = useState(false);
     const [isResetPasswordRequested, setIsResetPasswordRequested] = useState(false);
+    const [message, setMessage] = useState("");
+    const [messageIsHidden, setMessageIsHidden] = useState(true);
     const isMounted = useRef(true);
     const navigate = useNavigate();
     const { data } = MultiFetch();
@@ -29,58 +31,76 @@ function LoginPage() {
         setAuthenticationCode(event.target.value);
     }
     const handleAuthenticationRequest = async () => {
-        const authenticationRequestUrl = '/api/request/authenticate';
-        const requestBody = {email: email};
-        const response = await data(authenticationRequestUrl, "POST", requestBody );
-        if (response === true) {
-            setClientIsValid(true);
+        try{
+            const authenticationRequestUrl = '/api/request/authenticate';
+            const requestBody = {email: email, password: password};
+            const response = await data(authenticationRequestUrl, "POST", requestBody );
+            if (response === true) {
+                setClientIsValid(true);
+            }
+        } catch (e) {
+            console.error(e);
+            setMessage("Invalid email or password")
+            setMessageIsHidden(false);
         }
     }
     const handleSubmit = async (event) => {
-        event.preventDefault();
-        const authenticationUrl = '/api/authenticate';
-        const requestBody = {
-            email: email,
-            password: password,
-            role:"",
-            authenticationCode: authenticationCode
-        };
+        try {
+            event.preventDefault();
+            const authenticationUrl = '/api/authenticate';
+            const requestBody = {
+                email: email,
+                password: password,
+                role:"",
+                authenticationCode: authenticationCode
+            };
 
-        const response = await data(authenticationUrl,'POST',  requestBody);
+            const response = await data(authenticationUrl,'POST',  requestBody);
 
-        if (response.id) {
-            const token = response.token;
-            const id = response.id;
-            localStorage.setItem('userId', id);
-            localStorage.setItem('token', token);
-            navigate("/home");
+            if (response.id) {
+                const token = response.token;
+                const id = response.id;
+                localStorage.setItem('userId', id);
+                localStorage.setItem('token', token);
+                navigate("/home");
+            }
+        } catch (e) {
+            console.error(e.message);
+            setMessageIsHidden(false);
+            setMessage("Invalid email or password")
         }
-
     }
 
     return (
         <div className='pageContent'>
             <div>
-            <h1>Login</h1>
+            <h1 className="register-title">Login</h1>
+                <p display={messageIsHidden ? "none": "none"}>{message}</p>
             <form onSubmit={handleSubmit}>
-                <div>
+                <div className="inputBox">
                     <label htmlFor="email">Email:</label>
                     <input type="text" id="email" value={email} onChange={handleEmailChange} />
                 </div>
-                <div>
+                <div className="inputBox">
                     <label htmlFor="password">Password:</label>
                     <input type="password" id="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 {clientIsValid ?
-                    <><label htmlFor="code">Type sent Authentication Code:</label>
+                    <div className="inputBox">
+                        <label htmlFor="code">
+                            Type sent Authentication Code:
+                        </label>
                         <input
                         type="text"
                         id="code"
                         value={authenticationCode}
                         onChange={handleAuthenticationCodeChange}
                     />
-                    <button type="submit">Login</button></> :
+                    <button className="inputBox" type="submit">
+                        Login</button>
+                    </div> :
                     <button
+                        className="inputBox"
                         type="button"
                         onClick={() => handleAuthenticationRequest()}
                     >
@@ -89,18 +109,18 @@ function LoginPage() {
                 }
             </form>
             </div>
-            <div>
+            <div className="inputBox">
             <button onClick={() => handleGoogleLogin()}>Sign in With Google</button>
-            <h3 className="register"
+            <button className="register"
                 onClick={() =>  navigate("/register")}
             >
                 Not registered yet?
-            </h3>
-            <h3 className="register"
+            </button>
+            <button className="register"
                 onClick={() =>  setIsResetPasswordRequested(true)}
             >
                 Forgot your password ?
-            </h3>
+            </button>
             {isResetPasswordRequested ?
                 <div>
                     <Reset/>
