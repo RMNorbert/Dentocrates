d32fec431576ffd891890e3e9d239123d255b5c9
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-12 20:58:33 +0100

    feat: Add findByVerificationCodeAndEmail method to VerificationRepository

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/repository/client/verification/VerificationRepository.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/repository/client/verification/VerificationRepository.java
@@ -2,13 +2,16 @@ package com.rmnnorbert.dentocrates.repository.client.verification;
 
 import com.rmnnorbert.dentocrates.dao.verification.Verification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
 public interface VerificationRepository extends JpaRepository<Verification, Long> {
+    @Modifying
     @Query("DELETE FROM Verification v WHERE FUNCTION('HOUR_ADD', v.registrationTime, 12) < NOW()")
     int deleteExpiredVerifications();
+    Optional<Verification> findByVerificationCodeAndEmail(String verificationCode, String email);
     Optional<Verification> findByVerificationCode(String verificationCode);
 }
