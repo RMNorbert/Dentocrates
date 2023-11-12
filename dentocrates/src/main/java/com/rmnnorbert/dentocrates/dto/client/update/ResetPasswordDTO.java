693cf0fa5ca4f3b2136d4ef90b92fd868dc44be2
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-12 20:54:13 +0100

    typo: change record name Dto part to DTO

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/dto/client/update/ResetPasswordDTO.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/dto/client/update/ResetPasswordDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
-public record ResetPasswordDto(@NotBlank String verificationCode,
+public record ResetPasswordDTO(@NotBlank String verificationCode,
                                @NotBlank @Email String email,
                                @NotBlank @Size(min = 8) String password
 ) {
