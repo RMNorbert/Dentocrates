3715c98fbb8c0a14e88c25deac679ae34c943b73
Norbert Remenyi <remenyinorbert@gmail.com>
2023-11-20 18:05:35 +0100

    chore: replace create-db.sql create database with create if not exist

--- dentocrates/src/main/resources/db/create-db.sql
+++ dentocrates/src/main/resources/db/create-db.sql
@@ -1,1 +1,6 @@
-CREATE DATABASE dentocrates;
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_database WHERE datname = 'dentocrates') THEN
+            CREATE DATABASE dentocrates;
+        END IF;
+    END $$;
