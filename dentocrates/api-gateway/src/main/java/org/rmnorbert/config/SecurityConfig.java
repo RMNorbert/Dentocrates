b6ecf1930de367d7c84da36508fb8377a35fb0c8
Norbert Remenyi <remenyinorbert@gmail.com>
2023-12-14 19:45:18 +0100

    feat: add SecurityConfig to api gateway

---
+++ dentocrates/api-gateway/src/main/java/org/rmnorbert/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package org.rmnorbert.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
+        return serverHttpSecurity
+                                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                                 .authorizeExchange(authorizeExchangeSpec -> authorizeExchangeSpec.pathMatchers(
+                                "/eureka/**"
+                                ).authenticated()
+                                .anyExchange()
+                                .permitAll())
+                                .build();
+    }
+}
