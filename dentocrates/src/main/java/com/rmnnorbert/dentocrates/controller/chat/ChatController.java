91b2fc90f06265d60fdfd9747381c0119d3f033f
Norbert Remenyi <remenyinorbert@gmail.com>
2024-01-03 13:41:04 +0100

    Docs: Add Swagger annotations for methods in ChatController to improve API documentation

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/chat/ChatController.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/chat/ChatController.java
@@ -2,10 +2,22 @@ package com.rmnnorbert.dentocrates.controller.chat;
 
 import com.rmnnorbert.dentocrates.dto.chat.ChatDTO;
 import com.rmnnorbert.dentocrates.service.chat.ChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import static com.rmnnorbert.dentocrates.controller.ApiResponseConstants.*;
+
+@Tag(name = "Chat", description = "Chat management APIs")
 @RestController
 @RequestMapping("/chat")
 public class ChatController {
@@ -15,9 +27,17 @@ public class ChatController {
         this.chatService = chatService;
     }
 
-
+    @Operation(
+            summary = "Get the the chatbot provided response to the input",
+            description = "Get the the chatbot provided response by the user provided input. The response is a String.",
+            tags = { "Chat", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = OK_RESPONSE_CODE, description = "Successfully get the chatbot response for the user provided input.",
+                    content = { @Content(schema = @Schema(implementation = String.class), mediaType = RESPONSE_MEDIA_TYPE) }),
+            @ApiResponse(responseCode = INTERNAL_SERVER_ERROR_RESPONSE_CODE, description = INTERNAL_SERVER_ERROR_RESPONSE_CONTENT,
+                    content = { @Content(schema = @Schema(implementation = String.class), mediaType = RESPONSE_MEDIA_TYPE) })})
     @PostMapping
-    public ResponseEntity<String> getCustomerAppointments(@RequestBody ChatDTO input){
+    public ResponseEntity<String> getChatBotResponse(@RequestBody ChatDTO input){
         return chatService.getChatResponse(input);
     }
 }
