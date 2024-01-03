dd28b5a235b287240ea17ea0c76063d7e10ddae8
Norbert Remenyi <remenyinorbert@gmail.com>
2024-01-03 10:21:08 +0100

    Feat: Extending displayed information about clinic, add update button and add appointment card component to display the current day appointments

--- dentocrates/frontend/src/components/clinic/clinic/ClinicPage.jsx
+++ dentocrates/frontend/src/components/clinic/clinic/ClinicPage.jsx
@@ -1,15 +1,21 @@
 import "./ClinicPage.css";
+import { MdCheckBox } from "react-icons/md";
 import { useEffect, useState } from "react";
 import { Loading } from "../../lodaingPage/Loading";
 import { role , userId} from "../../../utils/token/TokenDecoder";
 import { MultiFetch } from "../../../utils/fetch/MultiFetch";
 import { useParams, useNavigate } from "react-router-dom";
 import {ReviewPage} from "../../review/Reviews";
+import AppointmentCard from "../calendar/AppointmentCard";
+import {dateFormatter} from "../../../utils/formatter/DateFormatter";
 
 export const ClinicPage = () => {
     const { id } = useParams();
     const { data } = MultiFetch();
-    const currentDate = new Date().toJSON();
+    const currentDateTime = new Date();
+    const month = String(currentDateTime.getMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(currentDateTime.getDate()).padStart(2, '0');
+    const currentDate = `${currentDateTime.getFullYear()}-${month}-${day}`;
     const navigate = useNavigate();
     const [isLoaded, setDataLoaded] = useState(false);
     const [isFetched, setIsFetched] = useState(false);
@@ -23,23 +29,20 @@ export const ClinicPage = () => {
         const clinicDataUrl = `/clinic/${id}`;
         const dentistDataUrl = `/dentist/`;
         const responseData = await data(clinicDataUrl);
-        setClinicData(await responseData);
+        setClinicData(responseData);
         const dentistResponse = await data(dentistDataUrl + responseData.dentistId);
         setDentistData(dentistResponse);
         const rating = await getRatingOfClinic();
         setRating(await rating);
     };
 
-    const updateAppointment = async (currentId, appearance) => {
-        const calendarDataUrl = '/calendar/';
-        const requestBody = {id:currentId, clinicId: id, dentistId: userId(), appeared:!appearance};
-        await data(calendarDataUrl,'PUT', requestBody);
-    }
     const getCalendarData = async () => {
+        const nextDay = currentDate.substring(0,currentDate.length - 2) +
+            String(currentDateTime.getDate() + 1).padStart(2, '0');
         const clinicCalendarDataUrl = `/calendar/clinic/${id}`;
         const calendarData = await data(clinicCalendarDataUrl);
         const appointmentsData = await calendarData.filter((appointment) =>
-            appointment.reservation >= currentDate).sort(
+            appointment.reservation >= currentDate && appointment.reservation < nextDay).sort(
             (a, b) => Number(a.reservation.substring(0,19)
                     .replace(/[T:-]/g, ""))
                 -
@@ -53,24 +56,13 @@ export const ClinicPage = () => {
         const ratingUrl = `/review/rating/clinic/${id}`;
         return await data(ratingUrl);
     }
+
     const getCustomerDetails = async() => {
         const customersDataUrl = `/client/all`;
         const responseData = await data(customersDataUrl);
         setCustomers(await responseData);
     };
 
-    const filterCustomerDetails = (customerId) => {
-        return customers.filter((customer) => customer.id === customerId)
-            .map((customer,index) => (
-                <div key={index}>
-                    <h2 className="listName listMargin">Name: {customer.firstName} {customer.lastName}</h2>
-                </div>
-            ))
-    };
-
-    const dateFormatter = (date) => {
-        return date.substring(0,16).replace("T"," ");
-    }
     const getCustomerAppointments = async () => {
         const customerAppointmentsDataUrl = `/calendar/customer/${userId()}`;
         const appointmentsData = await data(customerAppointmentsDataUrl);
@@ -109,16 +101,18 @@ export const ClinicPage = () => {
         return (
             <div className="clinic">
                 {clinicData ? (
-                    <div className="selected-clinic">
-                        <h1 className="selected-name">
+                    <div className="selected-clinic shadowBorder distanceHolder">
+                        <h1 className="selected-name shadowLightBorder" >
                             {clinicData.name}
                         </h1>
-                        <h2>
+                        <h2 id="content">
                             Location: {clinicData.zipCode} {clinicData.city} {clinicData.street}
                         </h2>
-                        <h3>{clinicData.clinicType.replaceAll("_"," ")}</h3>
-                        <h3>Open: {clinicData.openingHours}</h3>
-                        <h3>Dentist: Dr. {dentistData.firstName} {dentistData.lastName}</h3>
+                        <h3 id="content">{clinicData.clinicType.replaceAll("_"," ")}</h3>
+                        <h3 id="content">Open: {clinicData.openingHours}</h3>
+                        <h3 id="content">Phone: {clinicData.contactNumber}</h3>
+                        <h3 id="content">Dentist: Dr. {dentistData.firstName} {dentistData.lastName}</h3>
+                        <div>
                         { rating > 0 ?
                             <h4 className="clinic-rating">
                                 Rating: {rating}
@@ -126,40 +120,56 @@ export const ClinicPage = () => {
                             :
                             <></>
                         }
+                        {role() === "DENTIST" && clinicData.dentistId == userId() &&
+                            <button
+                                className="button shadowBorder updateButton clinic-updater"
+                                onClick={() => navigate(`/clinic/update/${id}`)}
+                            >
+                                Update clinic
+                            </button>
+                        }
+                        </div>
                     </div>
                 ) : (
                     <Loading />
                 )}
-                {role() === "DENTIST" && clinicData.dentistId === userId() ? (
-                    appointments.map((appointment, index) => (
-                        <>
-                            <div key={index}
-                                 className="listName listMargin customer-box"
-                            >
-                                {dateFormatter(appointment.reservation)}
-                                <input
-                                    className="fulfilled"
-                                    type="checkbox"
-                                    onChange={() => updateAppointment(appointment.id,appointment.appeared)}
-                                />
-                            {filterCustomerDetails(appointment.customerId)}
-                            </div>
-                        </>
+                {role() === "DENTIST" && clinicData.dentistId == userId() ? (
+                    appointments && appointments.map((currentAppointment, index) => (
+                        <div key={index}>
+                            <AppointmentCard id={id}
+                                             customers={customers}
+                                             appointments={currentAppointment}
+                                             dateFormatter={dateFormatter}/>
+                        </div>
                     ))
                 ) : (
                     <div>
-                        <button className="button" onClick={() => navigate("/calendar/" + id)}>Book an appointment</button>
+                        <button
+                            className="customer-box button shadowBorder"
+                            onClick={() => navigate("/calendar/" + id)}
+                        >
+                            Book an appointment
+                        </button>
+                        {customerAppointments.length > 0 && <h3 className="reviews-list">Appointments</h3>}
                         {customerAppointments.map((appointment, index) => (
                             <div key={index}
                             className="customer"
                             >
-                                <div className="customer-appointment">
-                                    {appointment.reservation.replace("T"," ")}
-                                    <input
-                                        className="fulfilled"
-                                        type="checkbox"
-                                        disabled={true}
-                                    />
+                                <div
+                                    className="listName listMargin customer-box shadowLightBorder"
+                                    id={appointment.appeared ? "appeared" : "not-appeared"}
+                                >
+                                   <strong>{appointment.reservation.replace("T"," ")}</strong>
+                                    {appointment.appeared ?
+                                        <MdCheckBox/>
+                                        :
+                                        <input
+                                            id="customer-appointment-card"
+                                            type="checkbox"
+                                            checked={appointment.appeared}
+                                            disabled={true}
+                                        />
+                                    }
                                 </div>
                             </div>
                         ))}
