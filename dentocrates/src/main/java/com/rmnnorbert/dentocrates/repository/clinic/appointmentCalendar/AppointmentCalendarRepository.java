04c2456516bf01d415051e00a20fc4a5bba57e09
Norbert Remenyi <remenyinorbert@gmail.com>
2024-01-03 11:15:23 +0100

    Chore: Update calendar appointment booking method by providing a date adjuster to exclude additional dependency for this purpose

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/repository/clinic/appointmentCalendar/AppointmentCalendarRepository.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/repository/clinic/appointmentCalendar/AppointmentCalendarRepository.java
@@ -11,6 +11,8 @@ import java.util.List;
 public interface AppointmentCalendarRepository extends JpaRepository<AppointmentCalendar,Long> {
     List<AppointmentCalendar> getAllByCustomer_Id(long id);
     List<AppointmentCalendar> getAllByClinic_Id(long id);
+    @Query("SELECT a FROM AppointmentCalendar a WHERE a.reservation >= CURRENT_DATE AND a.reservation <= CURRENT_DATE + 7 AND a.clinic.id = :id")
+    List<AppointmentCalendar> getTheWeekAllAppointmentsByClinicId(long id);
     @Query("SELECT a FROM AppointmentCalendar a WHERE a.reservation > :startOfLeave AND a.reservation < :endOfLeave AND a.clinic.id = :id")
     List<AppointmentCalendar> getAllByLeave(long id, LocalDateTime startOfLeave, LocalDateTime endOfLeave);
 }
