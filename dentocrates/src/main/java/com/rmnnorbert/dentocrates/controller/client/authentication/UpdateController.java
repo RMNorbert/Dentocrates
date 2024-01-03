d6bd3676031ad055ca37cfbcd294ed0f0e55f06d
Norbert Remenyi <remenyinorbert@gmail.com>
2024-01-03 13:39:54 +0100

    Docs: Add Swagger annotations for methods in UpdateController to improve API documentation

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/client/authentication/UpdateController.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/client/authentication/UpdateController.java
@@ -3,11 +3,21 @@ package com.rmnnorbert.dentocrates.controller.client.authentication;
 import com.rmnnorbert.dentocrates.dto.client.update.ResetPasswordDTO;
 import com.rmnnorbert.dentocrates.dto.client.verification.VerifyDTO;
 import com.rmnnorbert.dentocrates.service.client.ClientUpdaterService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.rmnnorbert.dentocrates.controller.ApiResponseConstants.*;
+
+@Tag(name = "Client update", description = "Client update management APIs")
 @RestController
 @RequestMapping("/update")
 public class UpdateController {
@@ -17,6 +27,19 @@ public class UpdateController {
         this.clientUpdateService = clientUpdateService;
     }
 
+    @Operation(
+            summary = "Update a client password",
+            description = "Update a client password with provided the ResetPasswordDTO. The response is a string indicating whether the password is successfully changed.",
+            tags = { "Client update", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = OK_RESPONSE_CODE, description = "Successfully change the client password.",
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = "Password change" + SUCCESSFUL_REGISTER_RESPONSE_CONTENT), mediaType = RESPONSE_MEDIA_TYPE) }),
+            @ApiResponse(responseCode = UNAUTHORIZED_RESPONSE_CODE, description = INVALID_CREDENTIALS_RESPONSE_CONTENT,
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = INVALID_CREDENTIALS_EXCEPTION_RESPONSE_CONTENT), mediaType = RESPONSE_MEDIA_TYPE) }),
+            @ApiResponse(responseCode = INTERNAL_SERVER_ERROR_RESPONSE_CODE, description = INTERNAL_SERVER_ERROR_RESPONSE_CONTENT,
+                    content = { @Content(schema = @Schema(implementation = String.class), mediaType = RESPONSE_MEDIA_TYPE) })})
     @PostMapping("/password")
     public ResponseEntity<String> resetPassword(
             @Valid @RequestBody ResetPasswordDTO dto
@@ -24,6 +47,22 @@ public class UpdateController {
         return clientUpdateService.resetPassword(dto);
     }
 
+    @Operation(
+            summary = "Update a client verified status",
+            description = "Update a client verified status to verified. The response is a string indicating whether the verification is successfully.",
+            tags = { "Client update", "post" })
+    @ApiResponses({
+            @ApiResponse(responseCode = OK_RESPONSE_CODE, description = "Successfully updated the client verified status.",
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = "Verification" + SUCCESSFUL_REGISTER_RESPONSE_CONTENT), mediaType = RESPONSE_MEDIA_TYPE) }),
+            @ApiResponse(responseCode = UNAUTHORIZED_RESPONSE_CODE, description = INVALID_CREDENTIALS_RESPONSE_CONTENT,
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = INVALID_CREDENTIALS_EXCEPTION_RESPONSE_CONTENT), mediaType = RESPONSE_MEDIA_TYPE) }),
+            @ApiResponse(responseCode = NOT_FOUND_RESPONSE_CODE, description = "Verification code" + NOT_FOUND_RESPONSE_CONTENT,
+                    content = { @Content(schema = @Schema(implementation = String.class),
+                            examples = @ExampleObject(value = "Verification code" + NOT_FOUND_RESPONSE_CONTENT), mediaType = RESPONSE_MEDIA_TYPE) }),
+            @ApiResponse(responseCode = INTERNAL_SERVER_ERROR_RESPONSE_CODE, description = INTERNAL_SERVER_ERROR_RESPONSE_CONTENT,
+                    content = { @Content(schema = @Schema(implementation = String.class), mediaType = RESPONSE_MEDIA_TYPE) })})
     @PostMapping("/verify")
     public ResponseEntity<String> verify(
             @Valid @RequestBody VerifyDTO dto
