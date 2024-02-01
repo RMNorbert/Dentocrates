5c0f36e1392714458a4c80e939f4186742222322
Norbert Remenyi <remenyinorbert@gmail.com>
2024-02-01 19:01:47 +0100

    Feat: Improve code documentation by adding comments to the ContactService class

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/service/client/communicationServices/ContactService.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/service/client/communicationServices/ContactService.java
@@ -6,12 +6,24 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ContactService {
+    /** The email address of the Dentocrates representative where the contact messages will be sent. */
     private final static String SUPPORT_TEAM_EMAIL_ADDRESS= System.getenv("SENDER_USERNAME");
     private final GMailerService emailService;
     @Autowired
     public ContactService(GMailerService emailService) {
         this.emailService = emailService;
     }
+
+    /**
+     * Sends a contact message to the Dentocrates support team.
+     *
+     * This method constructs the message content using the sender's email, message body,
+     * and subject from the provided ContactMessageDTO. It then sends the message to the
+     * Dentocrates support team's email address using the emailService.
+     *
+     * @param message The ContactMessageDTO containing sender's details, message, and subject.
+     * @return True if the message was sent successfully; false otherwise.
+     */
     public boolean sendMessage(ContactMessageDTO message) {
         try {
             String messageToSend = String.format("Contact email: %s :\n %s", message.sender(), message.message());
