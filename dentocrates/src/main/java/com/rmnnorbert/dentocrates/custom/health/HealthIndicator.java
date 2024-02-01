19064d1e4e3c9494941f2709765f25ede141a095
Norbert Remenyi <remenyinorbert@gmail.com>
2024-02-01 18:58:39 +0100

    Feat: Improve code documentation by adding comments to the HealthIndicator class

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/custom/health/HealthIndicator.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/custom/health/HealthIndicator.java
@@ -5,14 +5,25 @@ import org.springframework.boot.actuate.health.ReactiveHealthIndicator;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+/** Implements the ReactiveHealthIndicator interface to provide health check functionality. */
 @Component
 public class HealthIndicator implements ReactiveHealthIndicator {
+    /**
+     * Performs the health check and returns a Mono emitting the health status.
+     *
+     * @return A Mono emitting the health status.
+     */
     @Override
     public Mono<Health> health() {
         return checkHealth().onErrorResume(
                 ex -> Mono.just(new Health.Builder().down(ex).build())
         );
     }
+    /**
+     * Simulates a health check and returns an 'up' status.
+     *
+     * @return A Mono emitting the 'up' health status.
+     */
     private Mono<Health> checkHealth() {
         return Mono.just(new Health.Builder().up().build());
     }
