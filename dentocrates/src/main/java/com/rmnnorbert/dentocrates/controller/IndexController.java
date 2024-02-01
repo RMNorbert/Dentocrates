9ce6e8d281d0fc912a3b3a02e85b61b7cd79dda9
Norbert Remenyi <remenyinorbert@gmail.com>
2024-02-01 18:58:19 +0100

    Feat: Improve code documentation by adding comments to the IndexController class

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/IndexController.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/controller/IndexController.java
@@ -2,9 +2,14 @@ package com.rmnnorbert.dentocrates.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+/** Controller class for handling requests related to the application's index or home page. */
 @Controller
 public class IndexController {
+    /**
+     * Handles GET requests for the root ("/") endpoint and returns the path to the index.html file.
+     *
+     * @return The path to the index.html file.
+     */
     @GetMapping({"/"})
     public String index() {
         return "/index.html";
