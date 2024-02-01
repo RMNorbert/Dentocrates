a604e3c4d1d921522fd76f942413bce40a509cba
Norbert Remenyi <remenyinorbert@gmail.com>
2024-02-01 18:54:23 +0100

    Feat: Improve code documentation by adding comments to the OAuth2HelperService class

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/service/client/oauth2/OAuth2HelperService.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/service/client/oauth2/OAuth2HelperService.java
@@ -15,16 +15,56 @@ import org.springframework.web.client.RestTemplate;
 import java.text.ParseException;
 @Service
 public class OAuth2HelperService {
+    /** Google OAuth client ID obtained from environment variables */
     private final static String ID = System.getenv("OAUTH_ID");
+    /** Google OAuth client secret obtained from environment variables */
     private final static String SECRET = System.getenv("OAUTH_SECRET");
+    /** Redirect URI for Google OAuth callback obtained from environment variables */
     private final static String REDIRECT_URI = System.getenv("REDIRECT_URI");
+    /** URL for exchanging the authorization code with Google OAuth for an access token */
     private final static String OAUTH_TOKEN_URL = "https://www.googleapis.com/oauth2/v4/token";
+    /** Claim name for user email in the Google OAuth ID token */
     private final static String EMAIL_CLAIM = "email";
+    /** Claim name for username in the Google OAuth ID token */
     private final static String NAME_CLAIM = "name";
+    /** Index of the first name in the name claim split array */
     private final static int FIRST_NAME_INDEX = 0;
+    /** Index of the last name in the name claim split array */
     private final static int LAST_NAME_INDEX = 1;
 
-    public ResponseEntity<TokenResponse> getGoogleTokenResponse(String code) {
+    /**
+     * Retrieves OAuth credentials using the provided authorization code.
+     *
+     * @param generatedState The state value generated during the initial authentication request.
+     * @param state          The state value received in the callback, which should match the generated state.
+     * @param code           The authorization code received in the callback.
+     * @return An array containing OAuth credentials.
+     * @throws InvalidCredentialException If the state values do not match or if the token exchange fails.
+     */
+    public String[] getOauthCredentials(String generatedState , String state, String code) {
+        if (state.equals(generatedState)){
+            ResponseEntity<TokenResponse> responseEntity = getGoogleTokenResponse(code);
+            // Access the returned token and other fields from tokenResponse
+            if (responseEntity.getStatusCode() == HttpStatus.OK) {
+                TokenResponse tokenResponse = responseEntity.getBody();
+                String idToken = tokenResponse.getId_token();
+                return parseToken(idToken);
+            } else {
+                System.out.println("Token exchange failed. Status code: " + responseEntity.getStatusCode());
+                throw new InvalidCredentialException();
+            }
+        } else {
+            throw new InvalidCredentialException();
+        }
+    }
+
+    /**
+     * Retrieves the Google OAuth token response by exchanging the provided authorization code.
+     *
+     * @param code The authorization code received during the OAuth authorization flow.
+     * @return ResponseEntity containing the Google OAuth token response.
+     */
+    private ResponseEntity<TokenResponse> getGoogleTokenResponse(String code) {
         MultiValueMap<String, String> requestBody = buildRequestBody(code);
 
         // Set the headers for the request
@@ -43,7 +83,19 @@ public class OAuth2HelperService {
                 TokenResponse.class
         );
     }
-    public String[] parseToken(String idToken) {
+
+    /**
+     * Parses the provided ID token, extracting user attributes such as email, user ID, first name, and last name.
+     *
+     * @param idToken The ID token to be parsed.
+     * @return An array containing user attributes extracted from the ID token.
+     *         Index 0: User email
+     *         Index 1: User ID
+     *         Index 2: User first name
+     *         Index 3: User last name
+     * @throws InvalidCredentialException If there is an issue parsing the ID token.
+     */
+    private String[] parseToken(String idToken) {
         try {
             // Parse the ID token
             JWT jwt = JWTParser.parse(idToken);
@@ -63,22 +115,12 @@ public class OAuth2HelperService {
         throw new InvalidCredentialException();
     }
 
-    public String[] getOauthCredentials(String generatedState , String state, String code) {
-        if (state.equals(generatedState)){
-            ResponseEntity<TokenResponse> responseEntity = getGoogleTokenResponse(code);
-            // Access the returned token and other fields from tokenResponse
-            if (responseEntity.getStatusCode() == HttpStatus.OK) {
-                TokenResponse tokenResponse = responseEntity.getBody();
-                String idToken = tokenResponse.getId_token();
-                return parseToken(idToken);
-            } else {
-                System.out.println("Token exchange failed. Status code: " + responseEntity.getStatusCode());
-                throw new InvalidCredentialException();
-            }
-        } else {
-            throw new InvalidCredentialException();
-        }
-    }
+    /**
+     * Builds the request body for exchanging an authorization code in a Google OAuth token request.
+     *
+     * @param code The authorization code received during the OAuth authorization flow.
+     * @return MultiValueMap representing the request body with required parameters.
+     */
     private MultiValueMap<String, String> buildRequestBody(String code) {
         MultiValueMap<String, String> requestBody = new LinkedMultiValueMap<>();
         requestBody.add("code", code);
