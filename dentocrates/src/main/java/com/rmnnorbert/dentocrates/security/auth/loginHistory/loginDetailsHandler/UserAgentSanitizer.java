64a38a305a983bd1731b28e48b7761c45a69fcbf
Norbert Remenyi <remenyinorbert@gmail.com>
2024-02-01 18:49:05 +0100

    Feat: Improve code documentation by adding comments to the sanitizeUserAgent method

--- dentocrates/src/main/java/com/rmnnorbert/dentocrates/security/auth/loginHistory/loginDetailsHandler/UserAgentSanitizer.java
+++ dentocrates/src/main/java/com/rmnnorbert/dentocrates/security/auth/loginHistory/loginDetailsHandler/UserAgentSanitizer.java
@@ -4,6 +4,12 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserAgentSanitizer {
+    /**
+     * Sanitizes the user agent string by removing unwanted characters and converting to lowercase.
+     *
+     * @param userAgent The user agent string to be sanitized.
+     * @return The sanitized user agent string.
+     */
     public String sanitizeUserAgent(String userAgent) {
         if (userAgent == null) {
             return "";
